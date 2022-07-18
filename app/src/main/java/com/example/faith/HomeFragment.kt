86a50bffbd023f01305a3fea062f4f5933cfaf1c
Robbe Kerkaert <robbe.kerkaert@student.hogent.be>
2022-07-18 10:06:52 +0200

    Fixed databinding errors

---
+++ app/src/main/java/com/example/faith/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.faith
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.databinding.DataBindingUtil
+import com.example.faith.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
+        binding.loginButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_loginFragment)
+        }
+        return binding.root
+    }
+}
