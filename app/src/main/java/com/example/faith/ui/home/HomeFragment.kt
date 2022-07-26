be020ef97889a3a35f0d4b20d78acdfef0ccde00
Robbe Kerkaert <robbe.kerkaert@student.hogent.be>
2022-07-26 12:50:57 +0200

    Added menu for navigation

--- app/src/main/java/com/example/faith/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/faith/ui/home/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.faith.ui.home
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.ui.NavigationUI
 import com.example.faith.R
 import com.example.faith.databinding.FragmentHomeBinding
 
@@ -24,6 +23,18 @@ class HomeFragment : Fragment() {
         binding.loginButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
